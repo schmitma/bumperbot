650f0a4da3b00d525b699a40966f317daefb84bf
Maximilian Schmitt <max.schmitt@gmx.de>
2024-12-30 12:14:01 +0100

    Add code for simple service server

---
+++ bumperbot_cpp_examples/src/simple_service_server.cpp
@@ -0,0 +1,38 @@
+#include <bumperbot_msgs/srv/add_two_ints.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+using namespace std::placeholders;
+
+class SimpleServiceServer : public rclcpp::Node
+{
+ public:
+  SimpleServiceServer() : Node("simple_service_server")
+  {
+    service_ = create_service<bumperbot_msgs::srv::AddTwoInts>(
+        "add_two_ints", std::bind(&SimpleServiceServer::serviceCallback, this, _1, _2));
+
+    RCLCPP_INFO_STREAM(get_logger(), "Service add_two_ints ready");
+  }
+
+ private:
+  void serviceCallback(
+      std::shared_ptr<bumperbot_msgs::srv::AddTwoInts::Request> req,
+      std::shared_ptr<bumperbot_msgs::srv::AddTwoInts::Response> res)
+  {
+    RCLCPP_INFO_STREAM(get_logger(), "New request received a: " << req->a << " b: " << req->b);
+    res->sum = req->a + req->b;
+    RCLCPP_INFO_STREAM(get_logger(), "Response sum: " << res->sum);
+  }
+
+  rclcpp::Service<bumperbot_msgs::srv::AddTwoInts>::SharedPtr service_;
+};
+
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<SimpleServiceServer>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
