0c592a4ea7715ab0b0afb350d8ab5c5634f3b5b6
Maximilian Schmitt <max.schmitt@gmx.de>
2025-01-05 19:59:32 +0100

    Add code for arduino subscriber node

---
+++ src/bumperbot_firmware/bumperbot_firmware/simple_serial_receiver.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import rclpy
+import serial
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SimpleSerialReceiver(Node):
+    def __init__(self):
+        super().__init__("simple_serial_receiver")
+        self.pub_ = self.create_publisher(String, "serial_receiver", 10)
+        self.frequency_ = 0.01
+        self.get_logger().info("Publishing at %f Hz" % self.frequency_)
+
+        self.declare_parameter("port", "/dev/ttyACM0")
+        self.declare_parameter("baudrate", 115200)
+        self.port_ = self.get_parameter("port").value
+        self.baudrate_ = self.get_parameter("baudrate").value
+        self.arduino_ = serial.Serial(port=self.port_, baudrate=self.baudrate_)
+
+        self.timer_ = self.create_timer(self.frequency_, self.timerCallback)
+
+    def timerCallback(self):
+        if rclpy.ok() and self.arduino_.is_open:
+            data = self.arduino_.readline()
+            try:
+                data.decode("utf-8")
+            except Exception as e:
+                self.get_logger().error(f"{e}")
+                return
+
+            msg = String()
+            msg.data = str(data)
+            self.pub_.publish(msg)
+
+
+def main():
+    rclpy.init()
+
+    simple_serial_receiver = SimpleSerialReceiver()
+    rclpy.spin(simple_serial_receiver)
+
+    simple_serial_receiver.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
